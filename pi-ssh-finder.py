0a4f3d0e9183f35be010eb2936fbe423990f3337
David Struewing <david.struewing@gmail.com>
2016-01-15 09:43:09 -0500

    Added comments

--- pi-ssh-finder.py
+++ pi-ssh-finder.py
@@ -3,16 +3,18 @@ import os
 import signal
 import time
 
-output = 0
+# Ping the pi to get its IP address.
 p = subprocess.Popen(["ping raspberrypi.local"], stdout=subprocess.PIPE, shell=True)
 time.sleep(0.5)
 os.kill(p.pid, signal.SIGINT)
 
+# Get output from ping.
 out, err = p.communicate()
 
+# Extract the IP address from the output
+# and create a properly formatted ssh command.
 a,b = out.split("(")
 a,b = b.split(")")
-
 cmd = "ssh pi@%s" % a
 
 os.system(cmd)
